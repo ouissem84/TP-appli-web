34f3a967af2ff7b4f5d1b7cec228575f89075924
wissam <ouamriwissam@gmail.com>
2020-11-18 22:05:34 +0100

    miseajour

--- tp2/prog.js
+++ tp2/prog.js
@@ -1,27 +1,21 @@
 function validation(){
-  document.getElementById("error").innerHTML = "";
+  // ou faire une validation de tous les champs en une seule fois
+document.getElementById("error").innerHTML = "";
 
-  for(let item of document.querySelectorAll('.inputAW')){
-    if(item.value.length < 5){
-      // recuperation du label
-      var label =   document.querySelector("label[for="+item.id+"]").textContent;
-      // ajout du message d'erreur
-      document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
-      
-      // ajout de la classe CSS "display"
-      document.getElementById("error").classList.add("display");
-      document.getElementById("resultat").classList.remove("display");
-  
-    }
-      
-  
-    if(document.getElementById("error").innerHTML == ""){
-      var nom = document.getElementById("nom").value;
-      document.getElementById("resultat").innerHTML = `Bienvenue ${nom}`;
-      document.getElementById("error").classList.remove("display");
-      document.getElementById("resultat").classList.add("display");
-    }
-     
-  
-    
-  }
+for(let item of document.querySelectorAll('.inputAW')){
+if(item.value.length < 5){
+var label =   document.querySelector("label[for="+item.id+"]").textContent;
+document.getElementById("error").innerHTML += `Ce champ doit comporter au Minimum 5 caractères : ${label} <br/>`;
+document.getElementById("error").classList.add("display");
+document.getElementById("resultat").classList.remove("display")
+
+}
+}
+if(document.getElementById("error").innerHTML == ""){
+var nom = document.getElementById("nom").value
+document.getElementById("resultat").innerHTML = `bienvenue ${nom}`;
+document.getElementById("error").classList.remove("display")
+document.getElementById("resultat").classList.add("display")
+}
+
+}
