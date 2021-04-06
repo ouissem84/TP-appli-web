054cbc6476283780ff8957814603ed4146bc626a
wissam <ouamriwissam@gmail.com>
2021-04-06 11:45:37 +0200

    maj
    mise a jour

--- tp5/js/main.js
+++ tp5/js/main.js
@@ -25,7 +25,7 @@ window.onload = function () {
         },
 
         // 'mounted' est exécuté une fois l'application VUE totalement disponible
-        // Plus d'info. sur le cycle de vie d'une app VUE : 
+        // Plus d'info. sur le cycle de vie d'une app VUE :
         // https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
         mounted : function(){
             this.loaded = true;
@@ -40,10 +40,13 @@ window.onload = function () {
 
                 console.log('this.loaded:', this.loaded); // va afficher 'this.loaded: true'
             },
-            addCity: function (event) {  //Ajouter une méthode addCity à l'app VueJS
+            addCity: function (event) {
                 event.preventDefault(); // pour ne pas recharger la page à la soumission du formulaire
+
+                console.log('formCityName:',this.formCityName);
+                // A compléter dans la suite du TP
                 if(this.isCityExist(this.formCityName)){
-               this.messageForm = 'existe déjà';
+              this.messageForm = 'existe déjà';
             }
             else{
                 this.cityList.push({name : this.formCityName});
@@ -54,17 +57,54 @@ window.onload = function () {
                 // remise à zero du champ de saisie
                 this.formCityName = '';
             }
-          }
-        }
-});
-                console.log('formCityName:',this.formCityName);
-                // A compléter dans la suite du TP  
             },
-            remove: function (_city) {      
-                // A compléter dans la suite du TP          
-            }, 
-            meteo: function (_city) {  
-                // A compléter dans la suite du TP              
+            isCityExist: function (_cityName){
+
+           // la méthode 'filter' retourne une liste contenant tous les items ayant un nom égale à _cityName
+           // doc. sur filter : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/filter
+           if( this.cityList.filter(item =>
+                                       item.name.toUpperCase() == _cityName.toUpperCase()
+                                   ).length>0){
+               return true;
+           }
+           else{
+               return false;
+           }
+         },
+            remove: function (_city) {
+                // A compléter dans la suite du TP
+                // on utilise 'filter' pour retourne une liste avec tous les items ayant un nom différent de _city.name
+                 this.cityList = this.cityList.filter(item => item.name != _city.name);
+            },
+            meteo: function (_city) {
+                // A compléter dans la suite du TP
+                this.cityWeatherLoading = true;
+
+      // appel AJAX avec fetch
+      fetch('https://api.openweathermap.org/data/2.5/weather?q='+_city.name+'&units=metric&lang=fr&apikey=e8942ff19b37e0623f0de1c2137fb41b')
+          .then(function(response) {
+              return response.json();
+          })
+          .then(function(json) {
+              app.cityWeatherLoading = false;
+
+              // test du code retour
+              // 200 = OK
+              // 404 = city not found
+              if(json.cod === 200){
+                  // on met la réponse du webservice dans la variable cityWeather
+                  app.cityWeather = json;
+                  app.message = null;
+              }else{
+                  app.cityWeather = null;
+                  app.message = 'Météo introuvable pour ' + _city.name
+                                  + ' (' + json.message+ ')';
+              }
+          });
+
+
+
+
             }
         }
     });
